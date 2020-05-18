738f456177d37ff5ed1a4650dcec4044ba5dedcc
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-05-18 09:52:45 +0100

    Make package.sh print out it's statements

--- conda/package.sh
+++ conda/package.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -x
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh
 bash $HOME/miniconda.sh -b -p $MINICONDA_DIR
 source $MINICONDA_DIR/bin/activate $MINICONDA_DIR
+conda --version
 conda config --set always_yes yes
 conda config --set anaconda_upload yes
 conda install anaconda conda-build
